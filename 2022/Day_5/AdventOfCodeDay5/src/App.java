02c6b827358f16ed908b54918550bcfb06b42ca7
Tarun Chhabria <tarun.chhabria.work@gmail.com>
2022-12-23 19:10:40 +0530

    AoC 2022 Day 5 Solved

--- 2022/Day_5/AdventOfCodeDay5/src/App.java
+++ 2022/Day_5/AdventOfCodeDay5/src/App.java
@@ -1,17 +1,14 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.util.ArrayDeque;
 import java.util.ArrayList;
-import java.util.Deque;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.Stack;
 
 public class App {
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         String stacksDesc = """
-    [D]   
-[N] [C]   
+    [D]
+[N] [C]
 [Z] [M] [P]
  1   2   3
 
@@ -19,80 +16,69 @@ move 1 from 2 to 1
 move 3 from 1 to 3
 move 2 from 2 to 1
 move 1 from 1 to 2
-""";
+                """;
 
         BufferedReader br = new BufferedReader(new FileReader("lib/day5.txt"));
 
-        int stackIndexLine = 8;
-        int numStacks = 9;
-
-        Deque<String> stackLines = new ArrayDeque<>();
-
-        List<Deque<String>> stacks = new ArrayList<>();
-
-        int lineCount = 0;
-        List<Integer> stackIdx = new ArrayList<>();
-
+        String fileInput = "";
         String line;
+        int lineCounter = 0;
         while ((line = br.readLine()) != null) {
-        // for (String line : stacksDesc.split("\n")) {
-
-            if (line.isBlank()) continue;
-
-            if(lineCount < stackIndexLine) {
-                stackLines.push(line);
-                lineCount++;
-                continue;
+            if (lineCounter == 0) {
+                fileInput = line;
+                lineCounter++;
+            } else {
+                fileInput = fileInput + "\n" + line;
             }
-
-            if (stackIdx.isEmpty()) {
-                for (int i = 1; i < numStacks + 1; i++) {
-                    stackIdx.add(line.indexOf("" + i));
-                }
-                lineCount++;
-                continue;
-            }
-
-            if (stacks.isEmpty()) {
-                for (int j = 0; j < numStacks; j++) {
-                    stacks.add(new ArrayDeque<>());
-                }
-
-                String stackLine;
-                do {
-                    stackLine = stackLines.pop();
-                    for (int j = 0; j < numStacks; j++) {
-                        if (stackIdx.get(j) < stackLine.length()) {
-                            String boxId = stackLine.substring(stackIdx.get(j), stackIdx.get(j) + 1);
-                            if (!boxId.isBlank()) {
-                                stacks.get(j).push(boxId);
-                            }
-                        }
-                    }
-                } while (!stackLines.isEmpty());
-            }
-
-            Pattern movePattern = Pattern.compile("^move (\\d+) from (\\d+) to (\\d+)$");
-            Matcher m = movePattern.matcher(line);
-            if (m.find()) {
-                int numToMove = Integer.parseInt(m.group(1));
-                int moveFrom = Integer.parseInt(m.group(2));
-                int moveTo = Integer.parseInt(m.group(3));
-                Deque<String> cratesMove = new ArrayDeque<>();
-                for (int l = 0; l < numToMove; l++) {
-                    cratesMove.push(stacks.get(moveFrom - 1).pop());
-                }
-                for (int l = 0; l < numToMove; l++) {
-                    stacks.get(moveTo - 1).push(cratesMove.pop());
-                }
-            }
-            lineCount++;
         }
 
-        for (int k = 0; k < numStacks; k++) {
-            System.out.print(stacks.get(k).pop());
+        // System.out.println(fileInput);
+
+        int stackIndexLine = 9;
+
+        String[] Parts = fileInput.split("\n\n");
+
+        String[] drawing = Parts[0].split("\n");
+        String[] moves = Parts[1].split("\n");
+        List<Stack<String>> crates = new ArrayList<Stack<String>>();
+
+        for (int i = 0; i < stackIndexLine; i++) {
+            Stack<String> tempST = new Stack<>();
+            crates.add(tempST);
         }
 
-        System.out.println("");
+        for (int i = drawing.length - 2; i >= 0; i--) {
+            int stringLength = drawing[i].length();
+            // System.out.println(stringLength);
+            // System.out.println(drawing[i]);
+            int StackNum = 0;
+            for (int j = 1; j < stringLength; j = j + 4) {
+                if (!Character.isWhitespace(drawing[i].charAt(j))) {
+                    crates.get(StackNum).push("" + drawing[i].charAt(j));
+                    // System.out.println(crates.get(StackNum).peek());
+                }
+                StackNum++;
+            }
+        }
+
+        for (int i = 0; i < moves.length; i++) {
+            String[] tokens = moves[i].split(" ");
+            int n = Integer.parseInt(tokens[1]);
+            int src = Integer.parseInt(tokens[3]);
+            int dtn = Integer.parseInt(tokens[5]);
+            String element = "";
+            for(int j = 1;j <= n;j++){
+                element = element + crates.get(src-1).pop();
+            }
+            // System.out.println(element);
+            for(int K = element.length()-1;K >= 0;K--){
+                crates.get(dtn-1).push(""+element.charAt(K));
+            }
+        }
+
+        for (int i = 0; i < crates.size(); i++) {
+            // System.out.println(i);
+            System.out.print(crates.get(i).peek());
+        }
     }
 }
